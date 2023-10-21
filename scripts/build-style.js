5a5db70078ccb4e9107c6c51ccf591fc8504c331
Raine Revere <raine@cybersemics.org>
2023-10-21 22:04:40 +0000

    Chrome extension.

---
+++ scripts/build-style.js
@@ -0,0 +1,7 @@
+import fs from 'fs/promises'
+
+// add !important to all styles
+const css = await fs.readFile('src/style.css', 'utf8')
+const cssImportant = css.replace(/;/g, ' !important;')
+await fs.mkdir('build', { recursive: true })
+await fs.writeFile('build/style.css', cssImportant)
