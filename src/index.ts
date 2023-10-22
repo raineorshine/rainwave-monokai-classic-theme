a2c88ba20a4bdd33541b5734195b4b09c86e5fd9
Raine Revere <raine@cybersemics.org>
2023-10-22 16:45:57 +0000

    Save setting to chrome storage.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-type ElementSpec =
-  | [string]
-  | [string, { [key: string]: string }]
-  | [string, { [key: string]: string }, string | ElementSpec[]]
+type Attributes = { [key: string]: string | ((e: Event) => void) }
+
+type ElementSpec = [string] | [string, Attributes] | [string, Attributes, string | ElementSpec[]]
 
 /** Continuously poll a function until it returns a value other than null or undefined. */
 const waitFor = <T>(f: () => T, pollFrequency = 100): Promise<NonNullable<T>> => {
@@ -27,7 +26,7 @@ const $x = (xpath: string) =>
   document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE).singleNodeValue
 
 /** Create an element. */
-const $$ = (tagSpec: string, attributes: { [key: string]: string } = {}, content?: string | ElementSpec[]) => {
+const $$ = (tagSpec: string, attributes: Attributes = {}, content?: string | ElementSpec[]) => {
   const [tag, ...classNames] = tagSpec.split('.')
   const el = document.createElement(tag)
 
@@ -38,7 +37,12 @@ const $$ = (tagSpec: string, attributes: { [key: string]: string } = {}, content
 
   // attributes
   Object.entries(attributes).forEach(([key, value]) => {
-    el.setAttribute(key, value)
+    if (key.startsWith('on')) {
+      const eventName = key.slice(2).toLowerCase()
+      el.addEventListener(eventName, value as (e: Event) => void)
+    } else {
+      el.setAttribute(key, value as string)
+    }
   })
 
   if (typeof content === 'string') {
@@ -60,8 +64,34 @@ const $s = (styles: { [key: string]: string | number | null | undefined }) => ({
     .join(' '),
 })
 
-const Settings = () =>
-  $$('div', {}, [
+/** Create a class class attribute from an array or object of class names. */
+const $c = (classNames: { [key: string]: boolean | null | undefined } | (string | null | undefined)[]) => {
+  // normalize array or object to [key, value] tuples
+  const entries = Array.isArray(classNames)
+    ? classNames.map(key => [key, !!key] as [string, boolean])
+    : Object.entries(classNames)
+  return {
+    class: entries
+      .filter(([_, value]) => value)
+      .map(([key, _]) => key)
+      .join(' '),
+  }
+}
+
+/** Settings component. */
+const Settings = async () => {
+  const { theme } = await chrome.storage.sync.get('theme')
+
+  /** Creates an onClick handler that sets the theme and updates the selected theme option. */
+  const toggleTheme = (theme: string) => (e: Event) => {
+    chrome.storage.sync.set({ theme }, () => {
+      const el = e.target as HTMLElement
+      document.querySelector('.setting_theme .selected')?.classList.remove('selected')
+      el.classList.add('selected')
+    })
+  }
+
+  return $$('div.setting_theme', {}, [
     ['div.setting_subheader', {}, 'Theme'],
     [
       'div.setting_group',
@@ -71,18 +101,34 @@ const Settings = () =>
           'div.multi_select',
           $s({ float: 'none' }),
           [
-            ['span.link', $s({ 'margin-left': 0 }), 'Default'],
-            ['span.link.selected', {}, 'Monokai Classic Theme'],
+            [
+              'span.link',
+              {
+                ...$c({ selected: theme === 'default' }),
+                ...$s({ 'margin-left': 0 }),
+                onClick: toggleTheme('default'),
+              },
+              'Default',
+            ],
+            [
+              'span.link',
+              {
+                ...$c({ selected: !theme || theme === 'monokai-classic' }),
+                onClick: toggleTheme('monokai-classic'),
+              },
+              'Monokai Classic Theme',
+            ],
           ],
         ],
       ],
     ],
   ])
+}
 
 ;(async () => {
   const settingsLink = await waitFor(() => $x("//*[contains(@class, 'menu')]//a[text()='Settings']"))
   settingsLink?.addEventListener('click', async () => {
     const settingsContent = await waitFor(() => $('.modal_container.open .content.main'))
-    settingsContent.appendChild(Settings())
+    settingsContent.appendChild(await Settings())
   })
 })()
